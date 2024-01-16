d03e7332eef9e756df22f88b8b91ce1c5fa986e9
François Mignon <mignon.francois@outlook.com>
2024-01-16 12:12:13 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario
+{
+    internal class Program
+    {
+        //Constantes globales
+        const int LIGNE = 5;
+        const int COLONNE = 7;
+        const int NBR_OF_BOMBS = 5;
+        const string UP = "U";
+        const string DOWN = "D";
+        const string RIGHT = "R";
+        const string LEFT = "L";
+        const string EXPECTED_ANSWERS = "RLUD";
+        const string GAME_OVER = "Bombe touchée - Vous avez perdu!";
+        const string GAME_WIN = "Félicitations, vous avez gagné !";
+
+        static void Main(string[] args)
+        {
+            //Variables locales
+            int[][] BombPositions;
+            int[] marioPosition;
+            //Instructions
+            marioPosition = InitialMarioPositions(LIGNE, COLONNE);
+            BombPositions = ComputeBombPositions(marioPosition);
+            ShowBombsPosition(BombPositions);
+            UpdateMarioPositions(marioPosition, BombPositions);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Appuie sur une touche pour fermer...");
+            Console.ReadKey();
+        }
+
+        public static int[] InitialMarioPositions(int ligne, int colonne)
+        {
+            //Variables locales
+            int[] marioPosition = { (ligne - 1) / 2, (colonne - 1) / 2 };
+            //Instructions
+            return marioPosition;
+        }
+
+        public static int[][] ComputeBombPositions(int[] marioPosition)
+        {
+            //Variables locales
+            int[][] BombPositions = new int[NBR_OF_BOMBS][];
+            int x;
+            int y;
+            //Instructions
+            //Instanciation complete de mon tableau
+            for (int i = 0; i < BombPositions.Length; i++)
+            {
+                BombPositions[i] = new int[] { -1, -1 };
+            }
+            for (int i = 0; i < BombPositions.Length; i++)
+            {
+                do
+                {
+                    x = Utilities.RandInt(0, COLONNE - 1);
+                    y = Utilities.RandInt(0, COLONNE - 1);
+                }
+                while (Utilities.isAlreadyInTab(x, y, marioPosition) || Utilities.isAlreadyInTab(x, y, BombPositions));
+
+                BombPositions[i][0] = x;
+                BombPositions[i][1] = y;
+            }
+
+            return BombPositions;
+        }
+
+        public static void ShowBombsPosition(int[][] BombPosition)
+        {
+            for (int i = 0; i < BombPosition.Length; i++)
+            {
+                //Console.Write(("(" + BombPosition[i][0] + "," + BombPosition[i][1] + ")"));
+
+                Console.Write("(");
+                Console.Write(BombPosition[i][0]);
+                Console.Write(",");
+                Console.Write(BombPosition[i][1]);
+                Console.Write(") ");
+            }
+            Console.WriteLine("\n------------");
+
+        }
+
+        public static void UpdateMarioPositions(int[] MarioPositions, int[][] BombsPos)
+        {
+            //Variables locales
+            String Move;
+            bool isMarioPositionWin;
+            //Instructions
+            do
+            {
+                Move = Utilities.getSpecificInput("Pos actu " + "(" + MarioPositions[0] + ", " + MarioPositions[1] + ") déplacement RLUD ?", EXPECTED_ANSWERS);
+                if (Move == UP)
+                {
+                    //MarioPositions[0] -= 1;
+                    MarioPositions[0] --;
+                }
+                else if (Move == DOWN)
+                {
+                    //MarioPositions[0] += 1;
+                    MarioPositions[0] ++;
+                }
+                else if (Move == RIGHT)
+                {
+                    //MarioPositions[1] += 1;
+                    MarioPositions[1] ++;
+                }
+                else if (Move == LEFT)
+                {
+                    //MarioPositions[1] -= 1;
+                    MarioPositions[1] --;
+                }
+                    
+                isMarioPositionWin = 0 <= MarioPositions[0] && MarioPositions[0] < LIGNE && 0 <= MarioPositions[1] && MarioPositions[1] < COLONNE;
+                //Console.WriteLine("Mario est sur une bombe ? "+ Utilities.isAlreadyInTab(MarioPositions[0], MarioPositions[1], BombsPos));
+                //Console.WriteLine($"Mario est sur x={MarioPositions[0]} et y = {MarioPositions[1]}");
+
+            } while (!Utilities.isAlreadyInTab(MarioPositions[0], MarioPositions[1], BombsPos) && isMarioPositionWin);
+            
+            Console.WriteLine(isMarioPositionWin ? GAME_OVER : GAME_WIN);
+                
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario
+{
+    public class Utilities
+    {
+        /// <summary>
+        /// Génere un nombre aléatoire entre un minimum et un maximum
+        /// </summary>
+        private static readonly Random rand = new Random();//Instancier en dehors de RandInt afin d'évité les même nombre aléatoire car crée trop vite avec les même valeur système
+        public static int RandInt(int min, int max)
+        {
+            return rand.Next(min, max);
+        }
+        /// <summary>
+        /// Cherche une paire de nombre déja présent dans un tableau de x lignes et 2 colonnes
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="tab"></param>
+        /// <returns>Retourne vrai si la paire et trouvée</returns>
+        public static bool isAlreadyInTab(int x, int y, int[][] tab)
+        {
+
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+
+                if (tab[i][0] == x && tab[i][1] == y)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+        /// <summary>
+        /// Cherche une paire de nombre déja présent dans un tableau de 1 lignes et 1 colonnes
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="tab"></param>
+        /// <returns></returns>
+        public static bool isAlreadyInTab(int x, int y, int[] tab)
+        {
+
+                if (tab[0] == x && tab[1] == y)
+                {
+                    return true;
+                }
+            return false;
+
+        }
+
+        public static String getSpecificInput(String msg, String expectedAnswers)
+        {
+            String strAnswer;
+            do
+            {
+                Console.Write(msg);
+                strAnswer = Console.ReadLine().Trim().ToUpper();
+                if (strAnswer.Length == 1 && expectedAnswers.ToUpper().IndexOf(strAnswer) >= 0)//si aucune lettre n'est trouvée dans la chaine, il renvoye -1, sinon, il renvoie l'index ou la lettre est trouvée (donc voilà pourquoi >= 0)
+                {
+                    return strAnswer;
+                }
+                else
+                {
+                    Console.WriteLine("Erreur de saisie, votre choix doit être parmi " + expectedAnswers + " une lettre seulement");
+                }
+            } while (true);
+        }
+    }
+}
